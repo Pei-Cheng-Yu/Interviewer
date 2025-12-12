a24ba959caab93d0ff6bc87055279026cb84a040
Meow <seansean988@gmail.com>
2025-12-13 00:07:25 +0800

    add temperature param for get_llm

--- backend/app/core/llm.py
+++ backend/app/core/llm.py
@@ -1,13 +1,15 @@
 from langchain_ollama import ChatOllama
 
 
-llm = ChatOllama(
-    model="gemma3:4b",
-    temperature=0,
-)
 
-def get_llm():
+def get_llm(temperature: int = 0):
     """
     Returns the configured LLM instance.
     """
+    
+    llm = ChatOllama(
+        model="gemma3:4b",
+        temperature=temperature,
+    )
+    
     return llm
